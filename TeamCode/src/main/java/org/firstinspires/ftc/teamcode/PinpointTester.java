5ef4156378e27ecae2a73019f5fcf8b0b384f078
NinjaPenguin <noiwont@no.com>
2026-06-13 10:29:45 -0600

    Gave pinpointTester the odometry pod offsets.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PinpointTester.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PinpointTester.java
@@ -3,12 +3,15 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
 @TeleOp (group = "enigma", name = "PinpointTester")
 public class PinpointTester extends LinearOpMode {
     public GoBildaPinpointDriver pinpoint;
     public void runOpMode(){
         pinpoint = hardwareMap.get(GoBildaPinpointDriver.class, "PinPoint");
         pinpoint.resetPosAndIMU();
+        pinpoint.setOffsets(6.75, 2.9375, DistanceUnit.INCH);
         waitForStart();
         while (opModeIsActive()){
             telemetry.addData("Pinpoint position",pinpoint.getPosition());
